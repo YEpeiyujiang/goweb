4b801965a848e04b74db86736373ac752d78bcff
YEpeiyujiang <1783644194@qq.com>
2023-06-19 17:17:39 +0800

    响应页面

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 type IndexData struct {
@@ -15,9 +17,20 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Base-Type", "application/json")
 	var indexData IndexData
 	indexData.Title = "叶培宇的个人博客"
-	indexData.Desc = "两天完成"
+	indexData.Desc = "即将崛起的超级大佬"
 	jsonStrc, _ := json.Marshal(indexData)
 	w.Write(jsonStrc)
+}
+
+func indexHtml(w http.ResponseWriter, r *http.Request) {
+	var indexData IndexData
+	indexData.Title = "叶培宇的个人博客"
+	indexData.Desc = "即将崛起的超级大佬"
+	t := template.New("index.html")
+	//拿到当前的路径
+	path, _ := os.Getwd()
+	t, _ = t.ParseFiles(path + "/template/index.html")
+	t.Execute(w, indexData)
 }
 
 func main() {
@@ -28,9 +41,13 @@ func main() {
 	server := http.Server{ //绑定服务端口信息
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/", index)
+	//http.HandleFunc("/", index)
+	//if err := server.ListenAndServe(); err != nil {
+	//	log.Println(err)
+	//}
+
+	http.HandleFunc("/index", indexHtml)
 	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
 	}
-
 }
