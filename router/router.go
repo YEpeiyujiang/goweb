c95de9196a8662546caba585a3b49ad134cdfb77
YEpeiyujiang <1783644194@qq.com>
2023-06-20 20:43:25 +0800

    修改图片版

--- router/router.go
+++ router/router.go
@@ -11,5 +11,5 @@ func Router() {
 	//1、返回一个页面view 2、返回数据json 3、返回静态资源
 	http.HandleFunc("/", views.HTML.Index)
 	http.HandleFunc("/api/v1/post", api.API.SaveAndUpdatePost)
-	http.Handle("/resource/", http.StripPrefix("/resource/", http.FileServer(http.Dir("public/public/resource/"))))
+	http.Handle("/resource/", http.StripPrefix("/resource/", http.FileServer(http.Dir("public/resource/"))))
 }
