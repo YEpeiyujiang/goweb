8aaf7989fd4fe128370d36f52ce3f3ab99c5e762
YEpeiyujiang <1783644194@qq.com>
2023-06-23 16:33:58 +0800

    测试

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	//程序入口，一个项目 只能有一个入口
 	//web程序，http协议 ip port
 	server := http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr: "127.0.0.1:8089",
 	}
 	router.Router()
 	if err := server.ListenAndServe(); err != nil {
@@ -24,3 +24,5 @@ func main() {
 	}
 
 }
+
+//我测试测试
